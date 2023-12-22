74ff2afc40f578989576b04dd45abcd7681e2c58
Emzzy241 <emzzyoluwole@gmail.com>
2023-12-22 06:47:44 +0100

    add get + set for Car security system and passed Test

--- CarDealership.Tests/modelTests/CarTests.cs
+++ CarDealership.Tests/modelTests/CarTests.cs
@@ -10,6 +10,7 @@ namespace CarDealership.Tests
     [TestClass]
     public class CarTests
     {
+        // First Test: Testing for Creating Instance of car class
         [TestMethod]
         public void CarConstructor_CreatesInstanceOfCar_Car()
         {
@@ -18,6 +19,7 @@ namespace CarDealership.Tests
             
         }
 
+        // Second Test: Test to get car model (an auto-implemented property)
         [TestMethod]
         public void GetCarModel_GetsTheCarModel_String()
         {
@@ -33,6 +35,7 @@ namespace CarDealership.Tests
 
         }
 
+        // Third Test: Test to set car model (an auto-implemented property)
         [TestMethod]
         public void SetCarModel_SetsCarModel_Void()
         {
@@ -47,34 +50,84 @@ namespace CarDealership.Tests
             Assert.AreEqual(newCarModel, myCar.CarModel);
         }
 
+        // Fourth Test: Test to get car engine type(an auto-implemented property) 
         [TestMethod]
         public void GetCarEngineType_ReturnsCarEngineType_String()
         {
             // Arrange
-            Car myCar = new Car("Toyota Camry", "b", "c", true, "e");
-            string expectedCarModel = "Toyota Camry";
+            Car myCar = new Car("Toyota Camry", "Electric vehicle", "c", true, "e");
+            string expectedCarModel = "Electric vehicle";
 
             // Act
-            string returnedCarModel = myCar.CarModel;
+            string returnedCarModel = myCar.CarEngineType;
 
             // Assert
             Assert.AreEqual(expectedCarModel, returnedCarModel);
 
         }
 
+        // Fifth Test: Test to set car engine type(an auto-implemented property)
         [TestMethod]
-        public void GetCarSecuritySystem_SetsSecuritySystemOfCar_Void()
+        public void SetCarEngineType_SetsCarEngineType_Void()
         {
             // Arrange
             Car myCar = new Car("a", "b", "c", true, "e");
-            string setCarEngineType = "Toyota Camry";
+            string setCarEngineType = "Electric vehicle";
 
             // Act
-            myCar.CarSecuritySystem = setCarEngineType;
+            myCar.CarEngineType = setCarEngineType;
 
             // Assert
-            Assert.AreEqual(setCarEngineType, myCar.CarSecuritySystem);
+            Assert.AreEqual(setCarEngineType, myCar.CarEngineType);
         }
+
+        // Sixth Test: Test to get car's security system (an autoo-implemented property)
+        [TestMethod]
+        public void GetCarSecuritySystem_ReturnsCarSecuritySystem_String()
+        {
+            // Arrange
+            Car myCar = new Car("a", "b", "Immobilization and Alarm security system", true, "e");
+            string expectedCarSecuritySystem = "Immobilization and Alarm security system";
+
+            // Act
+            string returnedCarSecuritySystem = myCar.CarSecuritySystem;
+
+            // Assert
+            Assert.AreEqual(expectedCarSecuritySystem, returnedCarSecuritySystem);
+        }
+
+        // Seventh Test: Test to se my car's security system(an auto-implemented property)
+        [TestMethod]
+         public void SetCarSecuritySystem_SetsCarSecuritySystem_Void()
+        {
+            // Arrange
+            Car myCar = new Car("a", "b", "c", true, "e");
+            string setCarSecuritySystem = "Alarm";
+
+            // Act
+            myCar.CarSecuritySystem = setCarSecuritySystem;
+
+            // Assert
+            Assert.AreEqual(setCarSecuritySystem, myCar.CarSecuritySystem);
+        }
+
+        // [TestMethod]
+        // public void GetCarIsDriverless_GetsWhetherCarIsDriverless_Bool()
+        // {
+        //     // Arrange
+        //     Car myCar = new Car("a", "b", "c", false, "e");
+        //     bool expectedCarIsDriverless = false;
+
+        //     // Act
+        //     bool returnedCarIsDriverless = myCar.CarIsDriverless;
+
+        //     // Assert
+
+            
+        // }
+
+
+
 
 
 
